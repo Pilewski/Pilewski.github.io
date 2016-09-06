6c6c297c1ed19466cb970e6ec8e299a3e428027f
Alex Pilewski <pilewski.alex@gmail.com>
2016-09-06 14:51:58 -0600

    button selector functinality

--- scripts.js
+++ scripts.js
@@ -1,4 +1,12 @@
 $(document).ready(function() {
-
-
+$('#random').on('click', function(){
+  randomButton();
+});
+$('#reset').on('click', function(){
+  resetButton();
+});
+function resetButton() {
+  var backgroundColor = $('.background')
+  backgroundColor.css( 'backgroundColor' )
+};
 });
