5d20206b8c341a448fc72f122e38c3d025a8ae2f
Alex Pilewski <pilewski.alex@gmail.com>
2016-09-06 15:08:07 -0600

    random button functionality / random button generator

--- scripts.js
+++ scripts.js
@@ -4,6 +4,20 @@ $('#random').on('click', function(){
   randomButton();
 });
 
+function randomButton() {
+  var backgroundColor = $('.background');
+  backgroundColor.css( 'backgroundColor', randomHex());
+}
+
+function randomHex() {
+  var hex = "#";
+  var colorArray = [0,1,2,3,4,5,6,7,8,9,'a','b','c','d','e','f'];
+  for (var i = 0; i < 6; i++) {
+    hex += colorArray[Math.floor(Math.random()*16)];
+  }
+  return hex;
+}
+
 $('#reset').on('click', function(){
   resetButton();
 });
@@ -12,5 +26,4 @@ function resetButton() {
   var backgroundColor = $('.background');
   backgroundColor.css( 'backgroundColor', 'white');
 }
-
 });
